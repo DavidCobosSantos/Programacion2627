7322c6a8357ec9701e6a5d355b8cef569ba62d51
DavidCobosSantos <david.cobos@iesnervion.es>
2026-09-25 14:24:57 +0200

    commt

--- UD1-EjerciciosParte1/src/ejercicios/Ej12.java
+++ UD1-EjerciciosParte1/src/ejercicios/Ej12.java
@@ -1,11 +1,20 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ej12 {
 	public static void main(String[] args) {
 		
-		
-		
-	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("¿Cuántos kilos de manzanas se han vendido?");
+	Double manzanas = sc.nextDouble();
+	System.out.println("¿Cuántos kilos de peras se han vendido?");
+	Double peras = sc.nextDouble();
+	Double rManzanas = manzanas * 2.35;
+	Double rPeras = peras * 1.95;
+	Double resultado = rManzanas + rPeras;
+	System.out.println("Importe total: " + resultado + "€");
+	sc.close();
 		
 	}
 }
